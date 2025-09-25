12d088ddbb30d870ead6a07ed4e20596e4fb9c65
David Philien <davidplgomes@gmail.com>
2025-09-25 16:12:43 -0300

    build fix5

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,5 @@
-import { Header } from "@/components/layout/Header";
-import Footer from '@/components/layout/Footer';
-import BlogHeroSection from '@/components/custom/BlogHeroSection';
-import BlogSection from '@/components/custom/BlogSection';
-import NewsletterSection from '@/components/custom/NewsletterSection';
+import BlogIndex from '@/components/custom/BlogIndex';
 
 export default function BlogPage() {
-  return (
-    <main className="relative bg-neutral-50">
-      <Header variant="dark" />
-      <BlogHeroSection />
-      <BlogSection />
-      <NewsletterSection />
-      <Footer />
-    </main>
-  );
+  return <BlogIndex />;
 }
