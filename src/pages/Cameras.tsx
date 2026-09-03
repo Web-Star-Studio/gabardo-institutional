7e0c383695cb57a63222bbb0588ae013e0cb0ce2
Lorenzo Schönwald Lima <informatica8@gabardo.local>
2026-09-03 11:56:46 -0300

    Vercel enchendo o saco por conta de variáveis nãp usadas. Arrumado.

--- src/pages/Cameras.tsx
+++ src/pages/Cameras.tsx
@@ -1,5 +1,3 @@
-import CamerasMap from '@/componentes/CamerasMap';
-import PortaoCam from '@/componentes/cams/Portao';
 import FrenteCam from '@/componentes/cams/Frente';
 
 export default function Cameras() {
